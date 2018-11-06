635fe9f0c80d844a17f20b0ae03e72e0c8cbe7fa
hoaithi216 <44794326+hoaithi216@users.noreply.github.com>
2018-11-06 11:57:34 +0700

    Add files via upload

---
+++ DecoratorPattern/src/CheeseDecorator.java
@@ -0,0 +1,18 @@
+
+public class CheeseDecorator extends PizzaDecorator {
+
+    public CheeseDecorator(IPizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public String doPizza() {
+        String type = mPizza.doPizza();
+        return type + addCheese();
+    }
+
+   
+    private String addCheese() {
+        return "+ Cheese";
+    }
+}
